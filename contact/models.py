acc1f351f41733e4a972634ac175765217251800
João Lucas <joao.lgomes1166@gmail.com>
2025-07-15 15:32:55 -0300

    Registrando meu model na área administrativa do Django

--- contact/models.py
+++ contact/models.py
@@ -9,3 +9,6 @@ class Contact(models.Model):
     phone = models.CharField(max_length=15)
     created_date = models.DateTimeField(default=timezone.now)
     description = models.TextField(blank=True)
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
